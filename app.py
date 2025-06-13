d82c548c1a1f5e1dae3a7718b537e7aa326ee876
Mateusz Wiśniewski <110550176+Vinechetti@users.noreply.github.com>
2025-06-13 19:26:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 model_path = "model/model.pth"
-model = torch.load(model_path, weights_only=True)
+model = torch.load(model_path, weights_only=False)
 model.eval()
 
 data_T = T.Compose(
